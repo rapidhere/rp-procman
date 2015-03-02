56bbdf08486c4a0ca75873fe1f7e3130d709fa18
rapidhere <rapidhere@gmail.com>
2015-03-02 16:59:10 +0800

    update .ycm_extra_conf.py so it can work

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -35,13 +35,6 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wc++98-compat',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
 # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
 # language to use when compiling headers. So it will guess. Badly. So C++
 # headers will be compiled as C headers. You don't want that so ALWAYS specify
@@ -78,6 +71,7 @@ flags = [
 
 # use qt-widget
 '-I', './src/widgets',
+'-fPIE',
 '-D_RPM_QT_WIDGET_VERSION',
 
 # use qt-qml
